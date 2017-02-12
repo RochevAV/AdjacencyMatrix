428b3f8b64bda258abf9d5fd9f2aadcf43b5b199
Aleksey Rochev <alexrochevs@gmail.com>
2017-02-12 19:55:03 +0400

    Get adjacency matrix from edges and fix small bugs

--- AdjacencyMatrix/AdjacencyMatrix/AdjacencyMatrix/Matrix.swift
+++ AdjacencyMatrix/AdjacencyMatrix/AdjacencyMatrix/Matrix.swift
@@ -8,42 +8,61 @@
 
 import Foundation
 
+struct Edge : CustomStringConvertible{
+    var vertex1 : UInt
+    var vertex2 : UInt
+    
+    var description : String {
+        return "\(vertex1):\(vertex2)"
+    }
+}
+
 class Matrix {
+    
+    // MARK: - Properties
     
     let matrix : [[UInt8]]
     
-    var countNodes : Int {
+    var countVertex : Int {
         return matrix.count
     }
     
     // MARK: - Init
     
-    init(fromString string: String) {
+    init(withMatrix string: String) {
         matrix = Matrix.matrixFrom(string)
     }
     
-    // MARK: - Nodes
-    
-    func nodes() -> [[String]] {
-        
-        var nodes : [[String]] = []
-        for (index, _) in matrix.enumerated() {
-            nodes.append(self.nodesFor(node: index))
-        }
-        
-        return nodes
+    init(withEdges edges:Array<Edge>) {
+        matrix = Matrix.matrixFrom(edges)
     }
     
-    func nodesFor(node : Int) -> [String] {
+    // MARK: - Edges
+    
+    func edges() -> [Edge] {
         
-        var nodes : [String] = []
-        for (index, item) in matrix[node].enumerated() {
-            if item > 0 {
-                nodes.append("\(node):\(index)")
+        var edges : [Edge] = []
+        for (index, _) in matrix.enumerated() {
+            
+            if let edge = self.edgesFor(vertex: index) {
+                edges += edge
+            }
+        }        
+        return edges
+    }
+    
+    func edgesFor(vertex : Int) -> [Edge]? {
+        
+        var edges : [Edge] = []
+        for (index, item) in matrix[vertex][0...vertex].enumerated() {
+            if item != 0 {
+                
+                let edge = Edge.init(vertex1: UInt(vertex) + 1, vertex2: UInt(index) + 1)
+                edges.append(edge)
             }
         }
         
-        return nodes
+        return edges.isEmpty ? nil : edges
     }
     
     // MARK: - Static Methods
@@ -70,6 +89,20 @@ class Matrix {
         return matrix
     }
     
+    private static func matrixFrom(_ edges: [Edge]) -> [[UInt8]] {
+        
+        let count = Matrix.countVertex(forEdges: edges)
+        var matrix = Matrix.emptyMatrix(count: count)
+        
+        for edge in edges {
+            let (i,j) = (Int(edge.vertex1) - 1, Int(edge.vertex2) - 1)
+            matrix[i][j] = 1
+            matrix[j][i] = 1
+        }
+        
+        return matrix
+    }
+    
     private static func uint8Array(from string:String) -> [UInt8] {
         let array = string.components(separatedBy: " ")
         return array.flatMap{UInt8($0)}
@@ -89,6 +122,27 @@ class Matrix {
         let end = start + chunkSize - 1
         
         return (start, end)
+    }
+    
+    private static func countVertex(forEdges edges:[Edge]) -> UInt{
+        
+        var count : UInt = 0
+        
+        for edge in edges {
+            let vertex = max(edge.vertex1, edge.vertex2)
+            count = max(count, vertex)
+        }
+        
+        return count
+    }
+    
+    private static func emptyMatrix(count:UInt) -> [[UInt8]]{
+        var matrix : [[UInt8]] = []
+        for _ in 0..<count {
+            let row : [UInt8] = Array(repeating: 0, count: Int(count))
+            matrix.append(row)
+        }
+        return matrix
     }
 }
 
--- AdjacencyMatrix/AdjacencyMatrix/AdjacencyMatrix/main.swift
+++ AdjacencyMatrix/AdjacencyMatrix/AdjacencyMatrix/main.swift
@@ -9,14 +9,28 @@
 import Foundation
 
 
-print("Lab 1 \n")
+print("Lab 1 \"Adjacency Matrix\"  \n")
 
-let matrix = Matrix(fromString: "0 1 0 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 0 1 1 0 0 0")
+
+print("Getting edges from adjacency matrix: \n")
+let matrix = Matrix(withMatrix: "0 1 0 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 0 1 1 0 0 0")
 
 print(matrix)
-print(matrix.nodes())
+print(matrix.edges())
 
+let edge1 = Edge(vertex1: 1, vertex2: 1)
+let edge2 = Edge(vertex1: 2, vertex2: 1)
+let edge3 = Edge(vertex1: 3, vertex2: 2)
+let edge4 = Edge(vertex1: 4, vertex2: 3)
+let edge5 = Edge(vertex1: 5, vertex2: 2)
+let edge6 = Edge(vertex1: 6, vertex2: 5)
 
+let edges = [edge1, edge2, edge3, edge4, edge5, edge6]
 
+let matrix2 = Matrix(withEdges: edges)
 
+print("-------------------------------------------------")
+print("\nGetting adjacency matrix from eges: \n")
+print(edges)
+print(matrix2)
 
