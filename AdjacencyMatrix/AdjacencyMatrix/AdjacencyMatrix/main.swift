7142b669c132608e3de4311fb665c633816ac743
Aleksey Rochev <alexrochevs@gmail.com>
2017-02-12 11:55:58 +0400

    init xcode project

---
+++ AdjacencyMatrix/AdjacencyMatrix/AdjacencyMatrix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdjacencyMatrix
+//
+//  Created by Aleksey Rochev on 12/02/2017.
+//  Copyright © 2017 AlekseyRochev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
